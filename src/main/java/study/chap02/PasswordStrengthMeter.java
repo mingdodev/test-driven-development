0a441f3fe90439b92f68488efc3cd7a1bdd93e3f
mingdodev <mingdo.dev@gmail.com>
2025-03-31 02:20:51 +0900

    chap02: 암호 검사기 REFACTOR
    - 메서드 추출
    - 아~~ 이거 처음에는 테스트 소스 폴더에 있어야 한댔져!! 마지막에 배포 대상으로 만들어 줘야 한다.

--- src/main/java/study/chap02/PasswordStrengthMeter.java
+++ src/main/java/study/chap02/PasswordStrengthMeter.java
@@ -5,15 +5,20 @@ public class PasswordStrengthMeter {
 
         if (s == null || s.isEmpty()) return PasswordStrength.INVALID;
 
-        int metCounts = 0;
-        if (s.length() >= 8) metCounts++;
-        if (meetsContainingNumberCriteria(s)) metCounts++;
-        if (meetsContainingUpperCaseCriteria(s)) metCounts++;
+        int metCounts = getMetCriteriaCounts(s);
 
         if (metCounts <= 1) return PasswordStrength.WEAK;
         if (metCounts == 2) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
+    }
+
+    private int getMetCriteriaCounts(String s) {
+        int metCounts = 0;
+        if (s.length() >= 8) metCounts++;
+        if (meetsContainingNumberCriteria(s)) metCounts++;
+        if (meetsContainingUpperCaseCriteria(s)) metCounts++;
+        return metCounts;
     }
 
     private boolean meetsContainingNumberCriteria(String s) {
