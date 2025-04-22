fd9c29ad54f4e2f47f7348fb592a0b778d33e251
mingdodev <mingdo.dev@gmail.com>
2025-04-22 23:45:42 +0900

    chap03: 예외 상황 처리
    - 가장 쉽고 예외적인 상황부터
    - 리팩토링: 파라미터 개수가 세 개 이상이면 객체로 바꾸자.
    - 실제로는 리팩토링도 점진적으로 진행한다. 새로운 메서드를 만든 뒤 테스트 통과하면 사용하지 않는 메서드 삭제하기

--- src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -8,17 +8,45 @@ import org.junit.jupiter.api.Test;
 public class ExpiryDateCalculatorTest {
 
     @Test
-    void 만원_납부하면_한달_뒤가_만료일이_된다() {
+    void 만원_납부하면_한_달_뒤가_만료일이_된다() {
 
         assertExpiryDate(
-                LocalDate.of(2025, 4, 22), 10_000,
+                PayData.builder()
+                        .billingDate(LocalDate.of(2025, 4, 22))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2025,5,22)
         );
     }
 
-    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
+    @Test
+    void 납부일과_한_달_뒤_일자가_같지_않다() {
+        assertExpiryDate(
+                PayData.builder()
+                        .billingDate(LocalDate.of(2025, 1, 31))
+                        .payAmount(10_000)
+                        .build(),
+                LocalDate.of(2025, 2, 28)
+        );
+        assertExpiryDate(
+                PayData.builder()
+                        .billingDate(LocalDate.of(2025, 5, 31))
+                        .payAmount(10_000)
+                        .build(),
+                LocalDate.of(2025, 6, 30)
+        );
+        assertExpiryDate(
+                PayData.builder()
+                        .billingDate(LocalDate.of(2024, 1, 31))
+                        .payAmount(10_000)
+                        .build(),
+                LocalDate.of(2024, 2, 29)
+        );
+    }
+
+    private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
         ExpiryDateCalculator cal = new ExpiryDateCalculator();
-        LocalDate realExpiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        LocalDate realExpiryDate = cal.calculateExpiryDate(payData.getBillingDate(), payData.getPayAmount());
 
         assertEquals(realExpiryDate, expectedExpiryDate);
     }
