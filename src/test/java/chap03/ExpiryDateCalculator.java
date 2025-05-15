9edae7033af1f0c2c28ed05bd0674950b61e531d
mingdodev <mingdo.dev@gmail.com>
2025-05-15 23:12:51 +0900

    chap03: 테스트 추가 및 리팩토링

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -1,19 +1,40 @@
 package chap03;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
+
     public LocalDate calculateExpiryDate(PayData payData) {
-        int addedMonths = 1;
+
+        int addedMonths = payData.getPayAmount() / 10_000;
         if (payData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-            if (payData.getFirstBillingDate().getDayOfMonth() !=
-            candidateExp.getDayOfMonth()) {
-                return candidateExp.withDayOfMonth(
-                        payData.getFirstBillingDate().getDayOfMonth()
-                );
-            }
+            return expiryDateUsingFirstBillingDate(payData, addedMonths);
+        } else {
+            return payData.getBillingDate().plusMonths(addedMonths);
         }
-        return payData.getBillingDate().plusMonths(addedMonths);
+    }
+
+    private LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths) {
+
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+        if (!isSameDayOfMonth(payData.getFirstBillingDate(), candidateExp)) {
+            final int dayLenOfCandidateMonth = lastDayOfMonth(candidateExp);
+            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+            if (dayLenOfCandidateMonth < dayOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandidateMonth);
+            }
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        } else {
+            return candidateExp;
+        }
+    }
+
+    private boolean isSameDayOfMonth(LocalDate date1, LocalDate date2) {
+        return date1 == date2;
+    }
+
+    private int lastDayOfMonth(LocalDate date) {
+        return YearMonth.from(date).lengthOfMonth();
     }
 }
